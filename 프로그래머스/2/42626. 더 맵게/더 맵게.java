f87a82e76ae5f2728ac2928e038a50c835a622cc
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-01-04 17:40:20 +0900

    [level 2] Title: 더 맵게, Time: 1515.86 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,33 +1,28 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
         
-        PriorityQueue<Integer> heap = new PriorityQueue<>(); // 최소 힙 구현
-        for (int sc : scoville) {
-            heap.add(sc);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville){
+            pq.add(s);
         }
         int cnt = 0;
-        if (heap.peek()>= K){
-            answer = 0;
-            return answer;
-        }
         
-        while(!heap.isEmpty()&&heap.size()>1){
+        while(pq.size()>1){
+            if(pq.peek() >= K) break;
             cnt++;
-            int min1 = heap.poll();
-            int min2 = heap.poll();
-            int newOne = min1+min2*2;
-            heap.add(newOne);
-            if (heap.peek()>=K){
-                answer = cnt;           
-                break;
-            }
+            int min1 = pq.poll();
+            int min2 = pq.poll();
+            int newOne = min1 + min2*2;
+            
+            pq.add(newOne);
         }
-        if (heap.peek() < K) answer = -1;
+        if (pq.peek() < K) {
+            return -1;
+        }
         
-        return answer;
+        return cnt;
     }
 }
