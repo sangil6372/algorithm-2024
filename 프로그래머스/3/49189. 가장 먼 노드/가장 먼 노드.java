0b815a2958af66b82e6e3f95a072d11db95990ce
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-01-06 16:49:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 86.57 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,51 +1,55 @@
 import java.util.*;
 
 class Solution {
+    
+    static List<List<Integer>> graph; // static 구현체 
+    
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0 ;i <= n;i++){
-            graph.add(new ArrayList<>());
+        graph = new ArrayList<>(); // 우측에 생략하는 이유
+        
+        for (int i = 0; i <= n; i++){
+            graph.add(new ArrayList<>()); //없어도 get이 가능한 이유? -> 나 바본가
         }
         
-        // 간선 추가 
         for (int[] e : edge){
             graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]); // 양방향 추가 
+            graph.get(e[1]).add(e[0]);
         }
         
-        // 최장 노드를 구하기 위해 최단거리가 필요함! 
-        
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, -1);
-        dist[1] = 0;  // 각 노드에서 모든 노드 이건 좀 어려운데 1번 노드에서만 기준으로 하면 간단할 듯?
-        
+        // 이제 큐를 이용해서 BFS를 구현하고 최단거리를 저장하는 리스트에 담자 
+        List<Integer> routes = new ArrayList<>();
+       
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,-1); // -1은 미방문을 의미   boolean과 int 결정 조건
+        
+        queue.add(1);  // 아 큐는 현재 상태만 넣는건가?
+        dist[1] = 0;    
         
         while(!queue.isEmpty()){
-            int curNode = queue.poll();
-            for (int node : graph.get(curNode)){
-                if (dist[node] == -1){ // 아직 방문하지 않은 노드 
-                    dist[node] = dist[curNode]+1; // 이렇게 최단 거리를 갱신
-                    queue.add(node);
+            int curNode = queue.poll();     // 현재 노드가 의미하는 바?
+            
+            for (int next : graph.get(curNode)){  // 이걸 또 next로 해야 되는 이유? 
+                if (dist[next]== -1){
+                    dist[next] = dist[curNode]+1;
+                    queue.add(next);
                 }
-            }         
-        }
-        
-        int max = 0;
-        int count = 0;    
-    
-        for (int i = 1;i <= n;i++){
-            if (max < dist[i]){
-                max = dist[i];
-                count = 1;
             }
-            else if (max == dist[i]) count++;
         }
+        int cnt = 0;
+        int maxDist = 0;
+        for (int d : dist){
+            if (d > maxDist){
+                maxDist = d;
+                cnt = 1;
+            }else if (d == maxDist) cnt++;
+            
+            
+        }
+        answer = cnt;
         
-        
-        return count;
+        return answer;
     }
 }
