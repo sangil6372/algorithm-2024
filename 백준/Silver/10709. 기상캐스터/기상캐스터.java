f729ecfb1fc57b30e2f315511d304cba8ae01a51
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-01-24 20:18:00 +0900

    [Silver V] Title: 기상캐스터, Time: 144 ms, Memory: 16172 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -4,44 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	int H = Integer.parseInt(st.nextToken());
-    	int W = Integer.parseInt(st.nextToken());
-    	
-    	char[][] list =  new char[H][W];
-    	int[][] result = new int[H][W];
-    	
-    	for (int i = 0; i < H; i++) {
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+
+		char[][] sky = new char[H + 1][W + 1];
+
+		for (int i = 0; i < H; i++) {
+			sky[i] = br.readLine().toCharArray();
+
 			for (int j = 0; j < W; j++) {
-				result[i][j] = -1;
-			}
-		}
-    	
-    	for (int i = 0; i < H; i++) {
-			st =  new StringTokenizer(br.readLine());
-			char[] row = st.nextToken().toCharArray();
-    		for (int j = 0; j < W; j++) {
-				char temp = row[j];
-    			if (temp == 'c') {
-    				for (int k = j; k < W; k++) {
-						result[i][k] = k-j;
+				int steps = 0;
+				for (int k = j; k >= 0; k--) {
+					if (sky[i][k] == 'c') { // equals 비교
+						sb.append(steps+" ");
+						break;
 					}
-    			}
+					steps++;
+					if (k==0) {
+						sb.append(-1+" ");
+					}
+				}
 			}
-    		for (int j = 0; j < W; j++) {
-				System.out.print(result[i][j]+" ");
-			}
-    		System.out.println();
+			sb.append("\n");
 		}
-    	
-    	
-    	
-    	
-    	
-    	
-    }   
+		System.out.println(sb);
+	}
+
 }
